b140592daddc14cc705ea962fc15781e7e3e53d7
Jason-ice-SCUT <m13680464231@gmail.com>
2025-11-03 23:57:30 +0800

    Create QA_Generate() and Gradio

--- RAG/query.py
+++ RAG/query.py
@@ -9,42 +9,45 @@ tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
 LLM_MODEL = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B"
                                                  ,torch_dtype="auto",
                                                   device_map="auto")
+def QA_Generate(query: str) ->str:
+#query = "电动平衡车的安全要求是什么？"
+    chunks: list[str] = embedding.query_db(query)
+    prompt = "Please answer the question based on the following context:\n"
+    prompt += f"Query: {query}\n"
+    prompt += "Context:\n"   
+    for c in chunks:
+        prompt += c + "\n"
+        prompt += "-----\n"
+        
+    messages = [{"role": "user", "content": prompt}]    
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+        enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+    )
+    # Generate text using the language model. LLM_MODEL.device is the device to use for the model.
+    model_inputs = tokenizer([text], return_tensors="pt").to(LLM_MODEL.device)
+    # conduct text completion
+    generated_ids = LLM_MODEL.generate(
+        **model_inputs,
+        max_new_tokens=32768
+    )
+    # Extract the generated token IDs, excluding the input prompt tokens
+    output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
 
-query = "电动平衡车的安全要求是什么？"
-chunks: list[str] = embedding.query_db(query)
-prompt = "Please answer the question based on the following context:\n"
-prompt += f"Query: {query}\n"
-prompt += "Context:\n"   
-for c in chunks:
-    prompt += c + "\n"
-    prompt += "-----\n"
-       
-messages = [{"role": "user", "content": prompt}]    
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
-)
-# Generate text using the language model. LLM_MODEL.device is the device to use for the model.
-model_inputs = tokenizer([text], return_tensors="pt").to(LLM_MODEL.device)
-# conduct text completion
-generated_ids = LLM_MODEL.generate(
-    **model_inputs,
-    max_new_tokens=32768
-)
-# Extract the generated token IDs, excluding the input prompt tokens
-output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+    # parsing thinking content
+    try:
+        # rindex finding 151668 (</think>)
+        index = len(output_ids) - output_ids[::-1].index(151668)
+    except ValueError:
+        index = 0
 
-# parsing thinking content
-try:
-    # rindex finding 151668 (</think>)
-    index = len(output_ids) - output_ids[::-1].index(151668)
-except ValueError:
-    index = 0
+    thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+    content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+    merge_thinking_and_content = thinking_content + "\n" + content
 
-thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
-content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+    return merge_thinking_and_content
 
-print(f"Thinking content: {thinking_content}")
-print(f"Content: {content}")
+#print(f"Thinking content: {thinking_content}")
+#print(f"Content: {content}")
